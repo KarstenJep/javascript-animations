0438317f2930369eb689f388be49234337a3a2c6
Karsten Jepsen <kjepsen86@gmail.com>
2021-09-14 23:10:02 -0500

    functioning animated background

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cloudTwo = document.getElementById('cloud-two')
 const cloudThree = document.getElementById('cloud-three')
 const cloudFour = document.getElementById('cloud-four')
 const cloudFive = document.getElementById('cloud-five')
+const cloudSix = document.getElementById('cloud-six')
+
 // const javascript = document.getElementById('javascript')
 // const react = document.getElementById('react')
 // const graphql = document.getElementById('graphql')
@@ -17,16 +19,18 @@ function move() {
   // the '10' consistent.
   sun.style.bottom = 0 + incrementer * 0.1 + '%'
   sun.style.left = 28 + incrementer * -0.03 + '%'
-  cloudOne.style.bottom = 40 + incrementer * 0.12 + '%'
+  cloudOne.style.bottom = 30 + incrementer * 0.12 + '%'
   cloudOne.style.left = 75 + incrementer * 0.1 + '%'
-  cloudTwo.style.bottom = 80 + incrementer * 0.14 + '%'
-  cloudTwo.style.left = 70 + incrementer * 0.15 + '%'
-  cloudThree.style.bottom = 60 + incrementer * 0.1 + '%'
+  cloudTwo.style.bottom = 60 + incrementer * 0.14 + '%'
+  cloudTwo.style.left = 75 + incrementer * 0.15 + '%'
+  cloudThree.style.bottom = 35 + incrementer * 0.1 + '%'
   cloudThree.style.left = 0 + incrementer * -0.12 + '%'
-  cloudFour.style.bottom = 70 + incrementer * 0.16 + '%'
-  cloudFour.style.left = 20 + incrementer * -0.15 + '%'
-  cloudFive.style.bottom = 60 + incrementer * 0.2 + '%'
-  cloudFive.style.left = 60 + incrementer * 0.16 + '%'
+  cloudFour.style.bottom = 40 + incrementer * 0.16 + '%'
+  cloudFour.style.left = 15 + incrementer * -0.15 + '%'
+  cloudFive.style.bottom = 40 + incrementer * 0.2 + '%'
+  cloudFive.style.left = 45 + incrementer * 0.16 + '%'
+  cloudSix.style.bottom = 55 + incrementer * 0.2 + '%'
+  cloudSix.style.left = 14 + incrementer * -0.16 + '%'
 //   javascript.style.left = 20 + incrementer * -2.2 + '%'
 //   react.style.left = 26 + incrementer * -1.5 + '%'
 //   graphql.style.left = 32 + incrementer * -1 + '%'
